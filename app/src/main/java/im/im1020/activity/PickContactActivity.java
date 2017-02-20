a98e7fbe48c34454a71091958bc7c463745b0eb6
Mancy <519660797@qq.com>
2017-02-20 17:43:26 +0800

    选择联系人适配器和获取数据

--- app/src/main/java/im/im1020/activity/PickContactActivity.java
+++ app/src/main/java/im/im1020/activity/PickContactActivity.java
@@ -2,14 +2,132 @@ package im.im1020.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import im.im1020.R;
+import im.im1020.adapter.PickAdapter;
+import im.im1020.model.Model;
+import im.im1020.model.bean.PickInfo;
+import im.im1020.model.bean.UserInfo;
+import im.im1020.utils.ShowToast;
 
 public class PickContactActivity extends AppCompatActivity {
+
+    @InjectView(R.id.tv_pick_save)
+    TextView tvPickSave;
+    @InjectView(R.id.lv_pick)
+    ListView lvPick;
+
+
+    private PickAdapter adapter;
+
+    private List<PickInfo> pickInfos;
+    private PickAdapter adapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_contact);
+        ButterKnife.inject(this);
+
+        initView();
+
+        //获取数据
+
+        initData();
+
+        inirListener();
+
+
+    }
+
+
+    private void inirListener() {
+
+        //设置item的点击事件
+
+        lvPick.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                //获取itemd checkbox
+
+                CheckBox cbpick = (CheckBox) findViewById(R.id.cb_item_pick_contacts);
+
+                //对当前checkbox状态进行取反
+
+                cbpick.setChecked(!cbpick.isChecked());
+
+                PickInfo pickInfo = pickInfos.get(position);
+
+
+                //设置当前状态
+
+                pickInfo.setCheck(cbpick.isChecked());
+
+                adapter.refresh(pickInfos);
+
+            }
+        });
+
+
+    }
+
+    private void initData() {
+
+        //获取联系人
+
+        // 本地
+
+        List<UserInfo> contacts = Model.getInstance().getDbManager().
+                getContactDao().getContacts();
+        if (contacts == null) {
+
+            return;
+        }
+        if (contacts.size() == 0) {
+            ShowToast.show(this, "您还没有好友");
+
+
+        }
+
+
+        //转换数据
+
+        pickInfos = new ArrayList<>();
+
+        for (UserInfo userInfo : contacts) {
+            pickInfos.add(new PickInfo(userInfo, false));
+
+        }
+        adapter.refresh(pickInfos);
+
+    }
+
+    private void initView() {
+
+        adapter = new PickAdapter(this);
+
+        lvPick.setAdapter(adapter1);
+
+
+    }
+
+
+    @OnClick(R.id.tv_pick_save)
+    public void onClick() {
+
+
     }
 }
---
+++ app/src/main/java/im/im1020/model/bean/PickInfo.java
@@ -0,0 +1,38 @@
+package im.im1020.model.bean;
+
+/**
+ * Created by Mancy on 2017/2/20.
+ */
+
+public class PickInfo {
+
+    private UserInfo userInfo;
+
+    private boolean isCheck;
+
+    public PickInfo(){}
+
+
+    public PickInfo(UserInfo userInfo, boolean isCheck) {
+
+
+        this.userInfo = userInfo;
+        this.isCheck = isCheck;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public boolean isCheck() {
+        return isCheck;
+    }
+
+    public void setCheck(boolean check) {
+        isCheck = check;
+    }
+}
